af6de5ac8a403234d02f7546f05040452c0ae99d
Dean Byrne <deanbyrne95@gmail.com>
2019-06-28 12:11:52 +0100

    testing form validation

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -20,6 +21,10 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ' + form.valid);
   }
 
 }
